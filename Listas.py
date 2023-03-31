79cb93d8dc1768955acee95c8753e2ec95f92ada
unknown <Lucas.Ferreira1859@gmail.com>
2023-03-31 09:43:25 -0300

    Finalização exercício 4

--- Listas.py
+++ Listas.py
@@ -19,3 +19,18 @@ def calcularnotas (Lista3):
     Média = sum(Lista3) / QuantidadeDeNúmeros
     return Média
 print(calcularnotas(Lista3))
+
+#Exercício 4
+print('Consoantes e vogais')
+Lista4 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',]
+ListaDeVogais = ['a', 'e', 'i', 'o', 'u']
+ListaDeConsoantes = []
+consoantes = 0
+contador = 0
+for i in Lista4:
+    if not Lista4 [contador] in ListaDeVogais:
+        consoantes += 1
+        ListaDeConsoantes.append(Lista4[contador])
+    contador+=1
+print(ListaDeConsoantes)
+print(consoantes)
