55b4ecb493a0ea4c0ddda6cc8a1ace04ca692b0d
unknown <Lucas.Ferreira1859@gmail.com>
2023-04-13 10:24:56 -0300

    retirada a def que estava incompleta

--- Listas.py
+++ Listas.py
@@ -56,7 +56,3 @@ print("Números Pares", ListaPares)
 print('Notas dos Alunos')
 listaAlunos = [["Michael" [9, 10, 8, 10]],["Jimmy" [5, 6, 4, 3]],["Kimberly" [10, 7, 8, 8]],["Walter" [10, 9, 7, 10]],["Gustavo" [9, 6, 7, 10]], ["Jesse" [3, 2, 5, 4]],["Lalo" [7, 6, 8, 7]],["Skyler" [9, 8, 6, 10]],["Chuck" [10, 7, 5, 10]],["Hector" [6, 7, 5, 6]]]
 
-def calcularMédia (listaAlunos):
-    média = []
-    for aluno in listaAlunos:
-    média = 
