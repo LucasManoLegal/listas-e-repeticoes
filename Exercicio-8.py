2219d0d17a494dbfbb392cb5443470267ebde847
unknown <Lucas.Ferreira1859@gmail.com>
2023-04-13 11:34:36 -0300

    Exercício 8 finalizado

---
+++ Exercicio-8.py
@@ -0,0 +1,21 @@
+#Exercicio-8
+
+Idades = []
+Alturas = []
+for i in range(0,5):
+    print('Insira uma idade')
+    idade = input()
+    print('Insira uma altura (em metros)')
+    altura = input()
+    Idades.append(idade)
+    Alturas.append(altura)
+
+Idades.reverse()
+Alturas.reverse()
+
+contador = 0
+for i in Idades:
+    print('Pessoa ' + str(contador + 1) + ':' )
+    print('Idade: ' + str(i))
+    print('Altura: '+ str(Alturas[contador]))
+    contador += 1 
