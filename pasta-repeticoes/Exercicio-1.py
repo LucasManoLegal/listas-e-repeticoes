5334ff96e47303bc5f9365126007a8fc156f06d4
unknown <Lucas.Ferreira1859@gmail.com>
2023-04-14 09:53:02 -0300

    últimas alterações do exercício 1

--- pasta-repeticoes/Exercicio-1.py
+++ pasta-repeticoes/Exercicio-1.py
@@ -2,9 +2,9 @@
 
 index = 0
 while(index == 0):
-    print('Me informe uma nota (de 0 a 10)')
+    print("Digite uma nota (de 0 a 10)")
     valorInformado = int(input())
     if valorInformado >= 0 and valorInformado <= 10:
         index = 1
     else:
-        print('Valor Inválido')
+        print("Nota Inválida")
