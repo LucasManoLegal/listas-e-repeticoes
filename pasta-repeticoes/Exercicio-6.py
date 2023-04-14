fa4c4faea26d28037e5d58614a998d9bc382b213
unknown <Lucas.Ferreira1859@gmail.com>
2023-04-14 14:59:45 -0300

    Exercício 7 finalizado

---
+++ pasta-repeticoes/Exercicio-6.py
@@ -0,0 +1,14 @@
+#Exercício 6
+
+index = 0
+while(index < 20):
+    index += 1
+    print(index)
+
+index = 0
+Linha = ''
+while(index < 20):
+    index += 1
+    Linha += str(index)
+    Linha += ' '
+print(Linha)
